bae104fbb34468390a999dd5d4e6e8b438f7a53a
Michael Friedman <contact@michaeldavidfriedman.com>
2016-12-12 13:41:04 -0800

    Add the create subcommand

--- guests.js
+++ guests.js
@@ -5,24 +5,8 @@ const path = require('path');
 const guestsPath = path.join(__dirname, 'guests.json');
 
 const node = path.basename(process.argv[0]);
-const file = process.argv[1];
+const file = path.basename(process.argv[1]);
 const cmd = process.argv[2];
-// 
-// console.log(node);
-// console.log(file);
-// console.log(cmd);
-
-// fs.readFile(guestsPath, 'utf8', (err, data) => {
-//   if (err) {
-//     throw err;
-//   }
-//
-//   const guests = JSON.parse(data);
-//
-//   console.log(Array.isArray(guests));
-//
-//   console.log(guests);
-// });
 
 if (cmd === 'read') {
   fs.readFile(guestsPath, 'utf8', (err, data) => {
@@ -35,7 +19,34 @@ if (cmd === 'read') {
     console.log(guests);
   });
 }
+else if (cmd === 'create') {
+  fs.readFile(guestsPath, 'utf8', (readErr, data) => {
+    if (readErr) {
+      throw readErr;
+    }
+
+    const guests = JSON.parse(data);
+    const guest = process.argv[3];
+
+    if (!guest) {
+      console.error(`Usage: ${node} ${file} ${cmd} GUEST`);
+      process.exit(1);
+    }
+
+    guests.push(guest);
+
+    const guestsJSON = JSON.stringify(guests);
+
+    fs.writeFile(guestsPath, guestsJSON, (writeErr) => {
+      if (writeErr) {
+        throw writeErr;
+      }
+
+      console.log(guest);
+    });
+  });
+}
 else {
-  console.error(`Usage: ${node} ${file} read`);
+  console.error(`Usage: ${node} ${file} [read | create]`);
   process.exit(1);
 }
