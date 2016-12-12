f45d0c55a1754a4b572dfe2646264a62a125f7d1
Michael Friedman <contact@michaeldavidfriedman.com>
2016-12-12 13:36:17 -0800

    Add the read subcommand

--- guests.js
+++ guests.js
@@ -4,12 +4,38 @@ const fs = require('fs');
 const path = require('path');
 const guestsPath = path.join(__dirname, 'guests.json');
 
-fs.readFile(guestsPath, 'utf8', (err, data) => {
-  if (err) {
-    throw err;
-  }
+const node = path.basename(process.argv[0]);
+const file = process.argv[1];
+const cmd = process.argv[2];
+// 
+// console.log(node);
+// console.log(file);
+// console.log(cmd);
 
-  const guests = JSON.parse(data);
+// fs.readFile(guestsPath, 'utf8', (err, data) => {
+//   if (err) {
+//     throw err;
+//   }
+//
+//   const guests = JSON.parse(data);
+//
+//   console.log(Array.isArray(guests));
+//
+//   console.log(guests);
+// });
 
-  console.log(guests);
-});
+if (cmd === 'read') {
+  fs.readFile(guestsPath, 'utf8', (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    const guests = JSON.parse(data);
+
+    console.log(guests);
+  });
+}
+else {
+  console.error(`Usage: ${node} ${file} read`);
+  process.exit(1);
+}
