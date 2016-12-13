34771aef3dc83fbca47f6e92dcf1571b850b4259
Michael Friedman <contact@michaeldavidfriedman.com>
2016-12-13 11:57:44 -0800

    Refactor HTTP server to send different responses

--- server.js
+++ server.js
@@ -5,11 +5,16 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  const guests = ['Mary', 'Don'];
-  const guestsJSON = JSON.stringify(guests);
-
-  res.setHeader('Content-Type', 'application/json');
-  res.end(guestsJSON);
+  if (req.method === 'GET' && req.url === '/guests') {
+    const guests = ['Mary', 'Don'];
+    const guestsJSON = JSON.stringify(guests);
+    res.setHeader('Content-Type', 'application/json');
+    res.end(guestsJSON);
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not found');
+  }
 });
 const port = 8000;
 
