e3d35d7579af50c4ec11b65b5b5d428573404d23
Michael Friedman <contact@michaeldavidfriedman.com>
2016-12-13 13:43:33 -0800

    Send all guest records from a JSON file

--- server.js
+++ server.js
@@ -2,20 +2,33 @@
 
 'use strict';
 
+const fs = require('fs');
 const http = require('http');
+const path = require('path');
+
+const guestsPath = path.join(__dirname, 'guests.json');
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET' && req.url === '/guests') {
-    const guests = ['Mary', 'Don'];
-    const guestsJSON = JSON.stringify(guests);
-    res.setHeader('Content-Type', 'application/json');
-    res.end(guestsJSON);
+    fs.readFile(guestsPath, 'utf8', (err, guestsJSON) => {
+      if (err) {
+        console.error(err.stack);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Internal Service Error');
+
+        return;
+      }
+      res.setHeader('Content-Type', 'application/json');
+      res.end(guestsJSON);
+    });
   } else {
     res.statusCode = 404;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Not found');
   }
 });
+
 const port = 8000;
 
 server.listen(port, () => {
