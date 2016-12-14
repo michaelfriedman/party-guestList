8e6fe377f11b367a3e7e2e8d958fb6aa08e3f6df
Michael Friedman <contact@michaeldavidfriedman.com>
2016-12-14 11:59:53 -0800

    Send all guest records from the database

--- serverExpress.js
+++ serverExpress.js
@@ -2,6 +2,11 @@
 
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
+
+const guestsPath = path.join(__dirname, 'guests.json');
+
 const express = require('express');
 
 const app = express();
@@ -9,8 +14,14 @@ const app = express();
 app.disable('x-powered-by');
 
 app.get('/guests', (req, res) => {
-  const guests = ['Mary', 'Don'];
-  res.send(guests);
+  fs.readFile(guestsPath, 'utf8', (err, guestsJSON) => {
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+    const guests = JSON.parse(guestsJSON);
+    res.send(guests);
+  });
 });
 
 app.use((req, res) => {
